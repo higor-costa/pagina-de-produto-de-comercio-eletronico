23c9fa0dadb4ca55ceb20d9c9f4eb1f25530294b
Higor Costa <96428214+higor-costa@users.noreply.github.com>
2022-05-09 08:43:59 -0300

    Atualiza documentação

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Esta função faz com que a caixa de itens adicionados ao carrinho seja exibida/escondida quando o ícone do carrinho for clicado
+// Esta função faz com que a caixa de itens adicionados ao carrinho seja exibida/escondida e feche o menu mobile, caso ele esteja aberto, quando o ícone do carrinho for clicado
 function mostrarCaixa() {
     let caixaMensagem = document.getElementById("caixa-mensagem");
     if (caixaMensagem.style.display === "block") {
